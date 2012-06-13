dd68f19f1b3aca11e566dfe9d35d06cf0e0733cd
Matthieu Guillermin <matthieu.guillermin@gmail.com>
2012-06-13 21:33:44 +0200

    Cleaning...

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
-/* Author:
+/**
+ * Template for an alert box
+ * @type {String}
+ */
+var alertTemplate = '<div class="alert alert-{{type}} fade in out"><a class="close" data-dismiss="alert" href="#">&times;</a><h4 class="alert-heading">{{title}}</h4>{{message}}</div>';
 
-*/
+/**
+ * Template for displaying search results
+ * @type {String}
+ */
+var resultTemplate = "<h2>Total : {{total}}</h2> " +
+    "<p>Search took {{took}} ms</p>" +
+    "<ul>" +
+    "{{#hits.hits}}" +
+    '<li><a href="http://twitter.com/{{_source.user.screen_name}}">{{_source.user.screen_name}}</a> : <a href="http://twitter.com/{{_source.user.screen_name}}/status/{{_id}}">{{_source.text}}</a></li>' +
+    "{{/hits.hits}}" +
+    "</ul>";
 
+/**
+ * Retrieve configuration from the local storage
+ * @return {Object}
+ */
 var retrieveConfig = function(){
     var storedValues = store.getAll();
     return {
@@ -15,65 +33,71 @@ var retrieveConfig = function(){
     };
 };
 
-var es = elasticSearch(retrieveConfig());
-
-$('ul.nav li a').click(function(){
-    $('.target').hide();
-    $('#'+$(this).data('target')).show();
-    var parentLi = $(this).closest('li');
-    parentLi.siblings().removeClass('active');
-    parentLi.addClass('active');
-});
-
-$('#start-track form').submit(function(event){
-    var track = $(this).find('input[name="track"]').val();
-    var riverName = $(this).find('input[name="river"]').val();
-    es.createTwitterRiver(riverName, options.twitterOauth, track, 1, function(data){
-        console.log(data);
-        $('#start-track').hide();
-        $('#stop-track').show();
+/**
+ * Initialize navigation
+ */
+var nav = function(){
+    $('ul.nav li a').click(function(){
+        $('.target').hide();
+        $('#'+$(this).data('target')).show();
+        var parentLi = $(this).closest('li');
+        parentLi.siblings().removeClass('active');
+        parentLi.addClass('active');
     });
-    return false;
-});
+};
 
-$('#stop-track form').submit(function(){
-    var riverName = $(this).find('input[name="river"]').val();
-    es.deleteTwitterRiver(riverName, function(data){
-        console.log(data);
-        $('#stop-track').hide();
-        $('#start-track').show();
+/**
+ * Initialize tracking
+ */
+var tracking = function(){
+    $('#start-track form').submit(function(event){
+        var track = $(this).find('input[name="track"]').val();
+        var riverName = $(this).find('input[name="river"]').val();
+        es.createTwitterRiver(riverName, track, 1, function(data){
+            console.log(data);
+            $('#start-track').hide();
+            $('#stop-track').show();
+        });
+        return false;
     });
-    return false;
-});
 
-var resultTemplate = "<h2>Total : {{total}}</h2> " +
-    "<p>Search took {{took}} ms</p>" +
-    "<ul>" +
-    "{{#hits.hits}}" +
-    '<li><a href="http://twitter.com/{{_source.user.screen_name}}">{{_source.user.screen_name}}</a> : <a href="http://twitter.com/{{_source.user.screen_name}}/status/{{_id}}">{{_source.text}}</a></li>' +
-    "{{/hits.hits}}" +
-    "</ul>";
-
-$('#search form').submit(function(){
-    var index = $(this).find('input[name="index"]').val();
-    var q = $(this).find('input[name="query"]').val();
-    var from = $(this).find('input[name="from"]').val();
-    var size = $(this).find('input[name="size"]').val();
-    var query;
-    if (_.isEmpty(q)) {
-        query = es.buildQueryMatchAll(from, size);
-    } else {
-        query = es.buildQueryStringQuery(q, from, size);
-    }
-    es.search(index, null, query, function(data){
-        var result = $.mustache(resultTemplate, data);
-        $('#results').empty().append(result);
+    $('#stop-track form').submit(function(){
+        var riverName = $(this).find('input[name="river"]').val();
+        es.deleteTwitterRiver(riverName, function(data){
+            console.log(data);
+            $('#stop-track').hide();
+            $('#start-track').show();
+        });
+        return false;
     });
-    return false;
-});
+};
 
-var alertTemplate = '<div class="alert alert-{{type}} fade in out"><a class="close" data-dismiss="alert" href="#">&times;</a><h4 class="alert-heading">{{title}}</h4>{{message}}</div>';
+/**
+ * Initialize search
+ */
+var search = function(){
+    $('#search form').submit(function(){
+        var index = $(this).find('input[name="index"]').val();
+        var q = $(this).find('input[name="query"]').val();
+        var from = $(this).find('input[name="from"]').val();
+        var size = $(this).find('input[name="size"]').val();
+        var query;
+        if (_.isEmpty(q)) {
+            query = es.buildQueryMatchAll(from, size);
+        } else {
+            query = es.buildQueryStringQuery(q, from, size);
+        }
+        es.search(index, null, query, function(data){
+            var result = $.mustache(resultTemplate, data);
+            $('#results').empty().append(result);
+        });
+        return false;
+    });
+};
 
+/**
+ * Initialize config storage
+ */
 var configStorage = function(){
     if (store.disabled) {
         $('#config').prepend($.mustache(alertTemplate, {type: "error", title: "Warning!", message:"Local storage is not enabled. Configuration cannot be saved."})).alert();
@@ -101,6 +125,11 @@ var configStorage = function(){
 }
 
 $(document).ready(function(){
+    nav();
+    tracking();
+    search();
     configStorage();
 });
+
+var es = elasticSearch(retrieveConfig());
 
